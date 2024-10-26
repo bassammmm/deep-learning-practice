54de1edddc605482772e26ee7e0adc6564a7961c
Bassam <muhammadbassam73@gmail.com>
2024-10-26 16:46:19 +0500

    card image classifier

---
+++ pytorchex.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+
